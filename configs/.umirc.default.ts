a6bffacdf7dac101958121280ca740ce024d3525
fuRan <NgeKaworu@gmail.com>
2022-01-18 15:38:11 +0800

    Squashed 'src/js-sdk/' changes from e8751c5..c2c033f
    c2c033f fix: listen esc  exit fullscreen event
    b80576d feat: support native fullscreen hook
    86e894c feat: tableCard
    75a7d5e fix: fullscreen
    7ff8794 feat: forward ref
    4383ffe feat: 支持分片并发
    2c5b511 feat: export excel
    285482c feat: queryKey
    3e79c3a feat:
    109fd8a opt: chunk
    ae2672e Merge commit 'be05a2a9dccccca5a110658f46b20a7d194159ff'
    daa0428 opt: chunk
    6c0bd98 feat: mask
    
    git-subtree-dir: src/js-sdk
    git-subtree-split: c2c033fb64deacd20a4f1b26d2b5af1fc8a5e486

--- configs/.umirc.default.ts
+++ configs/.umirc.default.ts
@@ -22,8 +22,52 @@ export default defineConfig({
   sass: false,
   hash: true,
   runtimePublicPath: true,
-  externals: {
-    moment: 'moment',
-  },
+  externals: { moment: 'moment' },
   scripts: ['https://lib.baomitu.com/moment.js/latest/moment.min.js'],
+  analyze: {
+    analyzerMode: 'server',
+    analyzerPort: 8888,
+    openAnalyzer: true,
+    // generate stats file while ANALYZE_DUMP exist
+    generateStatsFile: false,
+    statsFilename: 'stats.json',
+    logLevel: 'info',
+    defaultSizes: 'gzip', // stat  // gzip
+  },
+  chunks: ['react', 'lodash', 'icons', 'antd', 'umi'],
+  chainWebpack: (config) => {
+    config.merge({
+      optimization: {
+        splitChunks: {
+          minChunks: 1,
+          cacheGroups: {
+            icons: {
+              name: 'icons',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/]@ant-design[\\/](icons|icons-svg)[\\/]/,
+              priority: 15,
+            },
+            antd: {
+              name: 'antd',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](antd|rc-.*)[\\/]/,
+              priority: 14,
+            },
+            react: {
+              name: 'react',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/,
+              priority: 13,
+            },
+            lodash: {
+              name: 'lodash',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/]lodash[\\/]/,
+              priority: 12,
+            },
+          },
+        },
+      },
+    });
+  },
 });
